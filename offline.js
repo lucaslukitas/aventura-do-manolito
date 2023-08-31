c51e7a5a83665172bf2b1f3c2a08eb4eb0d827e3
Lucas <lucaslukitas2006@gmail.com>
2023-08-30 23:14:16 -0300

    Base Commit

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1693445933,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/obj_collision.png",
+		"images/obj_moving_platform.png",
+		"images/obj_player-sheet0.png",
+		"images/obj_door-sheet0.png",
+		"images/tlb_middle.png",
+		"images/tlb_center.png",
+		"images/obj_corner-sheet0.png",
+		"images/obj_corner-sheet1.png",
+		"images/obj_enemy-sheet0.png",
+		"images/obj_enemy-sheet1.png",
+		"images/obj_enemy_limit-sheet0.png",
+		"images/obj_coin-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/obj_boss1-sheet0.png",
+		"images/obj_boss1-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
